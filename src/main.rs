aff8a32ecf81001c6b5976243dd91dee4deab3b1
Samuel Rembisz <sjrembisz07@gmail.com>
2022-08-06 19:26:12 -0400

    Moved to async model for finding last @Dote message

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use dotenv::dotenv;
 use lazy_static::lazy_static;
 use rand::Rng;
+use serenity::futures::future::join_all;
+use serenity::http::Http;
 use std::sync::atomic::AtomicBool;
+use std::sync::Arc;
 use std::{collections::HashMap, env};
 
 use serenity::{
@@ -13,6 +16,7 @@ use serenity::{
         },
         gateway::Ready,
         id::GuildId,
+        prelude::{ChannelId, GuildChannel},
     },
     prelude::*,
 };
@@ -66,93 +70,231 @@ impl EventHandler for Handler {
                 }
             };
             if command.data.name == "lastping" {
-                let mut content = String::new();
-                let mut ephemeral = false;
-                let mut found_message = None;
-                let dote_role = match GUILD_DOTE_ROLES.get(&guild_id) {
-                    Some(role) => format!("<@&{}>", role),
-                    None => {
-                        eprintln!("Could not find dote role for guild {}", guild_id);
-                        return;
-                    }
-                };
-                match ctx.cache.guild(guild_id) {
+                let mut errors = vec![]; // Vec<(Content, Ephemeral)>
+                let newest_message; // (RawMessage, Content, Ephemeral)
+                newest_message = match ctx.cache.guild(guild_id) {
                     Some(g) => match g.channels(&ctx.http).await {
                         Ok(channels) => {
-                            'top: for (channel_id, channel) in channels {
-                                let messages = match channel
-                                    .messages(&ctx.http, |retriever| retriever.limit(100))
-                                    .await
-                                {
-                                    Ok(messages) => messages,
-                                    Err(e) => {
-                                        eprintln!("Error getting messages for channel {:?} in guild {:?} with error {:?}", channel_id, guild_id, e);
-                                        continue;
+                            let channels: Vec<((ChannelId, GuildChannel), Arc<Http>)> = channels
+                                .iter()
+                                .map(|c| ((c.0.clone(), c.1.clone()), ctx.http.clone()))
+                                .collect();
+                            let mut messages = vec![]; // Vec<(RawMessage, Content, Ephemeral)>
+                            let mut message_async_handles = vec![];
+                            for ((channel_id, channel), http) in channels {
+                                let handle = tokio::spawn(async move {
+                                    let channel_id = channel_id.clone();
+                                    let channel = channel.clone();
+
+                                    let dote_role = match GUILD_DOTE_ROLES.get(&guild_id) {
+                                        Some(role) => format!("<@&{}>", role),
+                                        None => {
+                                            eprintln!(
+                                                "Could not find dote role for guild {}",
+                                                guild_id
+                                            );
+                                            return None;
+                                        }
+                                    };
+
+                                    let messages = match channel
+                                        .messages(&http, |retriever| retriever.limit(100))
+                                        .await
+                                    {
+                                        Ok(messages) => messages,
+                                        Err(e) => {
+                                            eprintln!("Error getting messages for channel {:?} in guild {:?} with error {:?}", channel_id, guild_id, e);
+                                            return None;
+                                        }
+                                    };
+                                    for message in messages {
+                                        if message.content.contains(&dote_role) {
+                                            let timestamp = message.timestamp.unix_timestamp();
+                                            let elapsed =
+                                                Utc::now().naive_utc().signed_duration_since(
+                                                    NaiveDateTime::from_timestamp(timestamp, 0),
+                                                );
+                                            let content = format!(
+                                                "Last @Dote message was at <t:{}:T> ({:.02} days ago, from user <@{}>)",
+                                                timestamp,
+                                                (elapsed.num_seconds() as f64) / (60.0 * 60.0 * 24.0),
+                                                message.author.id
+                                            );
+                                            let found_message = Some(message.to_owned());
+                                            return Some((found_message, content, false));
+                                        }
                                     }
-                                };
-                                for message in messages {
-                                    if message.content.contains(&dote_role) {
-                                        let timestamp = message.timestamp.unix_timestamp();
-                                        let elapsed = Utc::now().naive_utc().signed_duration_since(
-                                            NaiveDateTime::from_timestamp(timestamp, 0),
-                                        );
-                                        content = format!(
-                                            "Last @Dote message was at <t:{}:T> ({:.02} days ago, from user <@{}>)",
-                                            timestamp,
-                                            (elapsed.num_seconds() as f64) / (60.0 * 60.0 * 24.0),
-                                            message.author.id
-                                        );
-                                        found_message = Some(message);
-                                        break 'top;
-                                    }
-                                }
+                                    None
+                                });
+                                message_async_handles.push(handle);
                             }
-                            if found_message.is_none() && content.len() == 0 {
-                                content =
-                                    format!("No @Dote messages found in guild {:?}", guild_id);
-                                ephemeral = true;
+
+                            join_all(message_async_handles)
+                                .await
+                                .iter()
+                                .filter_map(|handle_result| match handle_result {
+                                    Ok(result) => result.clone(),
+                                    Err(e) => {
+                                        eprintln!("Error while joining message handle: {:?}", e);
+                                        None
+                                    }
+                                })
+                                .for_each(|(message, content, ephemeral)| {
+                                    if let Some(message) = message {
+                                        messages.push((message, content, ephemeral));
+                                    } else {
+                                        errors.push((content, ephemeral));
+                                    }
+                                });
+
+                            match messages
+                                .iter()
+                                .max_by(|(message_a, _, _), (message_b, _, _)| {
+                                    message_a
+                                        .timestamp
+                                        .unix_timestamp()
+                                        .cmp(&message_b.timestamp.unix_timestamp())
+                                }) {
+                                Some(message) => {
+                                    Some((message.0.clone(), message.1.clone(), message.2))
+                                }
+                                None => {
+                                    let content = format!(
+                                        "Failed to find newest message for guild {:?}",
+                                        guild_id
+                                    );
+                                    eprintln!("{}", content);
+                                    errors.push((content, true));
+                                    None
+                                }
                             }
                         }
                         Err(e) => {
-                            content = format!("Error getting channels: {:?}", e);
-                            ephemeral = true;
+                            let content = format!("Error getting channels: {:?}", e);
+                            let ephemeral = true;
                             eprintln!("{}", content);
+                            errors.push((content, ephemeral));
+                            None
                         }
                     },
                     None => {
                         eprintln!("Failed to find guild with id {}", guild_id);
                         return;
                     }
+                };
+
+                match newest_message {
+                    Some((ref message, ref content, ephemeral)) => {
+                        let color: i32 = rand::thread_rng().gen_range(0x000000..=0xffffff);
+                        let http = ctx.http.clone();
+
+                        match command
+                            .create_interaction_response(http, |response| {
+                                response
+                                    .kind(InteractionResponseType::ChannelMessageWithSource)
+                                    .interaction_response_data(|message| {
+                                        message
+                                            .embed(|embed| {
+                                                embed
+                                                    .title("Last @Dote")
+                                                    .description(content)
+                                                    .color(color)
+                                            })
+                                            .ephemeral(ephemeral)
+                                    })
+                            })
+                            .await
+                        {
+                            Ok(_) => match message.reply(&ctx.http, "Here").await {
+                                Ok(_) => (),
+                                Err(e) => eprintln!("Error replying to message: {:?}", e),
+                            },
+                            Err(e) => eprintln!("Error adding interaction response: {:?}", e),
+                        }
+                    }
+                    None => {
+                        let error = match errors.first() {
+                            Some((content, _)) => content.clone(),
+                            None => String::from("Unknown Error Occured"),
+                        };
+                        let color: i32 = rand::thread_rng().gen_range(0x000000..=0xffffff);
+                        match command
+                            .create_interaction_response(&ctx.http, |response| {
+                                response
+                                    .kind(InteractionResponseType::ChannelMessageWithSource)
+                                    .interaction_response_data(|message| {
+                                        message
+                                            .embed(|embed| {
+                                                embed.title("Error").description(error).color(color)
+                                            })
+                                            .ephemeral(true)
+                                    })
+                            })
+                            .await
+                        {
+                            Ok(_) => (),
+                            Err(e) => eprintln!("Error adding interaction response: {:?}", e),
+                        }
+                    }
                 }
 
-                match command
-                    .create_interaction_response(&ctx.http, |response| {
-                        response
-                            .kind(InteractionResponseType::ChannelMessageWithSource)
-                            .interaction_response_data(|message| {
+                if errors.len() > 0 {
+                    if newest_message.is_some() {
+                        match command
+                            .create_followup_message(&ctx.http, |message| {
                                 message
                                     .embed(|embed| {
                                         embed
-                                            .title(if ephemeral { "Error" } else { "Last @Dote" })
-                                            .description(content)
-                                            .color(
-                                                rand::thread_rng().gen_range(0x000000..=0xffffff),
-                                            )
+                                            .title("Followup Errors")
+                                            .description(format!(
+                                                "There were errors:\n{}",
+                                                errors
+                                                    .iter()
+                                                    .map(|(content, _)| format!("\t{}", content))
+                                                    .collect::<Vec<String>>()
+                                                    .join("\n")
+                                            ))
+                                            .color(0xFF0000)
                                     })
-                                    .ephemeral(ephemeral)
+                                    .ephemeral(true)
                             })
-                    })
-                    .await
-                {
-                    Ok(_) => {
-                        if let Some(message) = found_message {
-                            match message.reply(&ctx.http, "Here").await {
-                                Ok(_) => (),
-                                Err(e) => eprintln!("Error replying to message: {:?}", e),
-                            }
+                            .await
+                        {
+                            Ok(_) => (),
+                            Err(e) => eprintln!("Error adding followup message: {:?}", e),
+                        }
+                    } else {
+                        match command
+                            .create_interaction_response(&ctx.http, |response| {
+                                response
+                                    .kind(InteractionResponseType::ChannelMessageWithSource)
+                                    .interaction_response_data(|message| {
+                                        message
+                                            .embed(|embed| {
+                                                embed
+                                                    .title("Followup Errors")
+                                                    .description(format!(
+                                                        "There were errors:\n{}",
+                                                        errors
+                                                            .iter()
+                                                            .map(|(content, _)| format!(
+                                                                "\t{}",
+                                                                content
+                                                            ))
+                                                            .collect::<Vec<String>>()
+                                                            .join("\n")
+                                                    ))
+                                                    .color(0xFF0000)
+                                            })
+                                            .ephemeral(true)
+                                    })
+                            })
+                            .await
+                        {
+                            Ok(_) => (),
+                            Err(e) => eprintln!("Error sending error message: {:?}", e),
                         }
                     }
-                    Err(e) => eprintln!("Error adding interaction response: {:?}", e),
                 }
             }
         }
